4f7b47af4391b4b88c64ff5667be9f45303b334a
sonam kandari <sonamkandari2004@gmail.com>
2025-05-11 13:48:31 +0530

    yes

--- mistry_message/src/app/schemas/messageSchema.ts
+++
@@ -1,6 +0,0 @@
-import {z} from "zod"
-
-export const messageSchema =z.object({
-    content:z.string().min(10,{message:'Content must be at leat of 10 characters '})
-    .min(300,{message:'Content must be  no longer than 300 characters '})
-})   
