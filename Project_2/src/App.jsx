ed33283ca4bc92d1d309400e4361d10e439ed8da
sonam kandari <sonamkandari2004@gmail.com>
2025-05-18 23:24:08 +0530

    project2

---
+++ Project_2/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navigation from './components/Navigation'
+
+function App() {
+  return ( <div>
+    {/* the component which ve had created we have to use it here */}
+    {/* importing navigation component directly */}
+   <Navigation/>
+    
+  </div>
+   
+  );
+}
+
+export default App
---
+++ Project_2/src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+const Navigation = () => {
+  return (
+   <nav>
+    <div className="logo">
+        <img src="Images\logo.png"alt="do some coding"></img>
+    </div>
+    <ul>
+       <li> Home </li> 
+       <li> About </li> 
+       <li> Contact</li> 
+    </ul>
+   </nav>
+  )
+}
+
+export default Navigation
+
